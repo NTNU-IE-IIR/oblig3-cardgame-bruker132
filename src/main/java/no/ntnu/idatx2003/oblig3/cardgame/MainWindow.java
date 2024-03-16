990be4e5eded6bbbd4a3afab6c2910d8597cfd6b
Bakri <albkry132@gmail.com>
2024-03-16 17:25:11 +0100

    Added new class Controller and added new functionality on MainWindow class .

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/MainWindow.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/MainWindow.java
@@ -3,6 +3,7 @@ package no.ntnu.idatx2003.oblig3.cardgame;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -22,11 +23,16 @@ import java.util.List;
 
 public class MainWindow extends Application {
 
+
+
+
   private DeckOfCards deck;
   private HandOfCards hand;
   private ImageView cardBackImageView;
 
-  @Override
+
+
+
   public void start(Stage stage) throws Exception {
     FlowPane imageCardsBox = new FlowPane();
     imageCardsBox.setMaxWidth(400 * 5);
@@ -60,9 +66,32 @@ public class MainWindow extends Application {
     cardBackImageView.setFitWidth(100); // Set width to fit your card size
     cardBackImageView.setPreserveRatio(true);
 
+    // VBox for labels on the left side
+    VBox labelBox = new VBox(10);
+    labelBox.setPadding(new Insets(0, 10, 0, 10));
+
+
+    // Label for hearts on the left
+    Label heartsLabel = new Label("Hearts");
+    heartsLabel.setMinSize(Label.USE_PREF_SIZE, Label.USE_PREF_SIZE);
+
+    // Label for sum of cards on the left
+    Label sumLabel = new Label("Sum of cards");
+    sumLabel.setMinSize(Label.USE_PREF_SIZE, Label.USE_PREF_SIZE);
+
     // Label for Flush on the left
     Label flushLabel = new Label("Flush");
     flushLabel.setPrefSize(50, 50);
+
+    // Label for Queens of Spades
+    Label queensOfSpadesLabel = new Label("Queens of Spades");
+    queensOfSpadesLabel.setMinSize(Label.USE_PREF_SIZE, Label.USE_PREF_SIZE);
+
+    labelBox.getChildren().addAll(flushLabel, queensOfSpadesLabel, heartsLabel, sumLabel);
+    rootNode.setLeft(labelBox);
+
+
+
 
     dealHandButton.setOnAction(e -> {
       imageCardsBox.getChildren().clear();
@@ -73,23 +102,44 @@ public class MainWindow extends Application {
       List<ImageView> cardImageViews = getImagesFromHand(cards);
       imageCardsBox.getChildren().addAll(cardImageViews);
     });
-
+    // Button for checking if the hand is a flush and contains "Queen of Spades".
     checkHandButton.setOnAction(e -> {
       if (hand != null && hand.isFlush()) {
         flushLabel.setText("Flush: Yes");
       } else {
         flushLabel.setText("Flush: No");
       }
+
+      if (hand != null) {
+        if (hand.checkIfHasQueenOfSpades()) {
+          queensOfSpadesLabel.setText("Queen of Spades: Yes");
+        } else {
+          queensOfSpadesLabel.setText("Queen of Spades: No");
+        }
+      }
+      if (hand != null) {
+        List<String> heartCardNames = getCardNamesFromHand(hand.getHearts());
+        heartsLabel.setText("Hearts: " + String.join(", ", heartCardNames));
+      } else {
+        heartsLabel.setText("No hearts");
+      }
+        if (hand != null) {
+            sumLabel.setText("Sum of cards: " + hand.culculateSumOfCards());
+        } else {
+            sumLabel.setText("No cards");
+        }
     });
+
+
+
+
+
 
     // Adds the buttons to the buttonBox
     userBox.getChildren().addAll(dealHandButton, checkHandButton);
     rootNode.setRight(userBox);
 
-    // Adds the flushLabel to the left side of the BorderPane.
-    BorderPane.setAlignment(flushLabel, Pos.BOTTOM_LEFT);
-    BorderPane.setMargin(flushLabel, new Insets(0, 10, 0, 10)); // Adding margin to move it down
-    rootNode.setLeft(flushLabel);
+
 
     BorderPane.setAlignment(dealtCardsFlow, Pos.CENTER);
     BorderPane.setMargin(dealtCardsFlow, new Insets(0, 50, 50, 50)); // Adding margin to move it down
@@ -99,6 +149,15 @@ public class MainWindow extends Application {
     stage.setScene(scene);
     stage.show();
   }
+
+  private List<String> getCardNamesFromHand(Collection<PlayingCard> hearts) {
+    List<String> cardNames = new ArrayList<>();
+    for (PlayingCard card : hearts) {
+      cardNames.add(card.getAsString());
+    }
+    return cardNames;
+  }
+
 
   public static void main(String[] args) {
     launch(args);
@@ -117,4 +176,6 @@ public class MainWindow extends Application {
     }
     return cardImageViews;
   }
-}
+
+  }
+
